f1887091b7f07403b153ad33712d58be3842c1e1
Mogeko <zhengjunyi@live.com>
2022-07-05 17:28:19 +0200

    feat: setup swr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig, Middleware, SWRHook } from "swr";
+
+const mockMiddleware: Middleware = (useSWRNext: SWRHook) => {
+  return (key, fetcher, config) => {
+    const mockHost = "https://mogeko.github.io/book-exchange-mock";
+    return useSWRNext(mockHost + key, fetcher, config);
+  };
+};
+
+const swrConfig = {
+  use: [mockMiddleware],
+};
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig value={swrConfig}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 };
 
 export default MyApp;
