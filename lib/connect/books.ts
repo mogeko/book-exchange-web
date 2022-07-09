3859fac2709c6a3fdf1fdb37ed7d83550574ec7a
Mogeko <zhengjunyi@live.com>
2022-07-09 20:48:56 +0200

    feat: support query parameter

--- lib/connect/books.ts
+++ lib/connect/books.ts
@@ -1,7 +1,10 @@
 import useSWR, { type SWRResponse } from "swr";
 
-function useBooks() {
-  const { data, error }: SWRResponse<BookTypes[]> = useSWR("/books");
+function useBooks(queryParam: QueryParamType = {}) {
+  const query = queryParam
+    ? `/books?${handleQueryParam(queryParam)}`
+    : "/books";
+  const { data, error }: SWRResponse<BookTypes[]> = useSWR(query);
   return {
     books: data,
     isLoading: !error && !data,
@@ -9,13 +12,22 @@ function useBooks() {
   };
 }
 
-export function useBook(id: string | number) {
-  const { data, error }: SWRResponse<BookTypes> = useSWR(`/books/${id}`);
+export function useBook(id: string | number, queryParam: QueryParamType = {}) {
+  const query = queryParam
+    ? `/books/${id}?${handleQueryParam(queryParam)}`
+    : `/books/${id}`;
+  const { data, error }: SWRResponse<BookTypes> = useSWR(query);
   return {
     book: data,
     isLoading: !error && !data,
     isError: error,
   };
+}
+
+function handleQueryParam(queryParams: QueryParamType) {
+  return Object.entries(queryParams)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
 }
 
 interface BookTypes {
@@ -27,5 +39,23 @@ interface BookTypes {
   publisher: string;
   title: string;
 }
+
+type QueryParamSuffix =
+  | "eq"
+  | "ne"
+  | "lt"
+  | "gt"
+  | "lte"
+  | "gte"
+  | "contains"
+  | "startsWith"
+  | "endsWith";
+
+type QueryParamType = {
+  [key in `${keyof BookTypes}_${QueryParamSuffix}`]?: number | string;
+} & { [key in `${keyof BookTypes}_order`]?: "asc" | "desc" } & {
+  limit?: number | string;
+  offset?: number | string;
+};
 
 export default useBooks;
