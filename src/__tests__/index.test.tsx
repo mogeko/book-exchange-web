86cabe2a56b53daab444f3f3e01aa156308467c9
Mogeko <zhengjunyi@live.com>
2022-07-10 01:25:04 +0200

    test: add some tests for display

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,57 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import * as books from "@/lib/connect/books";
 import Home from "@/pages/index";
 
-const exampleBook = {
-  title: "This is an example book",
-  cover: "https://picsum.photos/seed/35613/1280/1920/",
-  description: "This is an example book",
-  published: "2020-1-1T23:59:59Z",
-  publisher: "This is an example publisher",
-  author: "No One",
-  id: 1,
-};
-
 describe("Home", () => {
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
-
   it("renders a home page", () => {
-    const res = { books: [exampleBook], isError: false, isLoading: false };
-    jest.spyOn(books, "default").mockImplementation(() => res);
     render(<Home />);
 
-    expect(books.default).toBeCalledWith({ limit: 50 });
     expect(
       screen.getByRole("heading", {
         name: /Bookworm/i,
       })
     ).toBeInTheDocument();
-  });
-
-  it("renders a home page then error", () => {
-    const res = { books: [], isError: true, isLoading: false };
-    jest.spyOn(books, "default").mockImplementation(() => res);
-    render(<Home />);
-
-    expect(books.default).toBeCalledWith({ limit: 50 });
-    expect(
-      screen.getByRole("heading", { name: /Bookworm/i })
-    ).toBeInTheDocument();
-    expect(screen.getByText("Network Error!")).toBeInTheDocument();
-  });
-
-  it("renders a home page then loading", () => {
-    const res = { books: [], isError: false, isLoading: true };
-    jest.spyOn(books, "default").mockImplementation(() => res);
-    const { container } = render(<Home />);
-
-    expect(books.default).toBeCalledWith({ limit: 50 });
-    expect(
-      screen.getByRole("heading", { name: /Bookworm/i })
-    ).toBeInTheDocument();
-    expect(container.querySelector("div.animate-pulse")).toBeInTheDocument();
   });
 });
--- src/layouts/display.tsx
+++ src/layouts/display.tsx
@@ -7,45 +7,48 @@ import React, { useState } from "react";
 
 const Display: React.FC<DisplayProps> = ({ title, queryParam }) => {
   const { books, isError, isLoading } = useBooks(queryParam);
-  const pages = Array.from({ length: Number(queryParam.limit) / 10 }, (_, i) =>
-    books?.slice(i * 10, (i + 1) * 10)
+  const pages = Array.from(
+    { length: Math.ceil(queryParam.limit / 10) },
+    (_, i) => books?.slice(i * 10, (i + 1) * 10)
   );
   const [page, setPage] = useState(0);
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-lg w-full p-1">{title}</h1>
-      <div className="flex flex-wrap gap-6 my-4">
+      <div className="flex flex-wrap gap-6 my-4 w-full min-h-[27.5rem]">
         {isError && <Alert.Error message="Network Error!" />}
         {isLoading
           ? Array.from({ length: 10 }, (_, i) => <Card.BookSkeleton key={i} />)
-          : pages[page]?.map((book) => <Card.Book key={book.id} {...book} />)}
+          : pages[page]?.map((book, i) => <Card.Book key={i} {...book} />)}
       </div>
-      <div className="btn-group">
-        <button
-          className="btn btn-xs"
-          onClick={() => setPage((page) => Math.max(0, page - 1))}
-        >
-          <FaCaretLeft />
-        </button>
-        {pages.map((_, i) => (
+      {pages?.length > 1 && (
+        <div className="btn-group">
           <button
-            className={"btn btn-xs" + (page === i ? " btn-active" : "")}
-            onClick={(e) => setPage(Number(e.currentTarget.value))}
-            key={i}
-            value={i}
+            className="btn btn-xs"
+            onClick={() => setPage((page) => Math.max(0, page - 1))}
           >
-            {i + 1}
+            <FaCaretLeft />
           </button>
-        ))}
-        <button
-          className="btn btn-xs"
-          onClick={() =>
-            setPage((page) => Math.min(pages.length - 1, page + 1))
-          }
-        >
-          <FaCaretRight />
-        </button>
-      </div>
+          {pages.map((_, i) => (
+            <button
+              className={"btn btn-xs" + (page === i ? " btn-active" : "")}
+              onClick={(e) => setPage(Number(e.currentTarget.value))}
+              key={i}
+              value={i}
+            >
+              {i + 1}
+            </button>
+          ))}
+          <button
+            className="btn btn-xs"
+            onClick={() =>
+              setPage((page) => Math.min(pages.length - 1, page + 1))
+            }
+          >
+            <FaCaretRight />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
@@ -53,7 +56,7 @@ const Display: React.FC<DisplayProps> = ({ title, queryParam }) => {
 interface DisplayProps {
   title: string;
   queryParam: {
-    limit: number | string;
+    limit: number;
   } & Omit<QueryParamType<keyof BookTypes>, "limit">;
 }
 
