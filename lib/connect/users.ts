5e686cbb9312958dd5cccd4d0b8099d93697c70d
Mogeko <zhengjunyi@live.com>
2022-07-09 22:31:38 +0200

    feat: Implement query params for users

--- lib/connect/users.ts
+++ lib/connect/users.ts
@@ -1,7 +1,11 @@
 import useSWR, { type SWRResponse } from "swr";
+import handleQueryParam, { type QueryParamType } from "@/lib/utils/queryTools";
 
-function useUsers() {
-  const { data, error }: SWRResponse<UserTypes[]> = useSWR("/users");
+function useUsers(queryParam: QueryParamType<keyof UserTypes> = {}) {
+  const query = queryParam
+    ? `/users?${handleQueryParam(queryParam)}`
+    : "/users";
+  const { data, error }: SWRResponse<UserTypes[]> = useSWR(query);
   return {
     users: data,
     isLoading: !error && !data,
@@ -9,8 +13,14 @@ function useUsers() {
   };
 }
 
-export function useUser(id: string | number) {
-  const { data, error }: SWRResponse<UserTypes> = useSWR(`/users/${id}`);
+export function useUser(
+  id: string | number,
+  queryParam: QueryParamType<keyof UserTypes> = {}
+) {
+  const query = queryParam
+    ? `/users/${id}?${handleQueryParam(queryParam)}`
+    : `/users/${id}`;
+  const { data, error }: SWRResponse<UserTypes> = useSWR(query);
   return {
     user: data,
     isLoading: !error && !data,
