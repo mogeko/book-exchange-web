24cf02c84ad2816e579ceac417510432549b999f
Mogeko <zhengjunyi@live.com>
2022-07-15 20:54:17 +0200

    feat: add useBooksInfinite to unlimited loading data

--- lib/connect/books.ts
+++ lib/connect/books.ts
@@ -1,11 +1,13 @@
 import useSWR, { type SWRResponse } from "swr";
+import useSWRInfinite, { type SWRInfiniteResponse } from "swr/infinite";
 import handleQueryParam, { type QueryParamType } from "@/lib/utils/queryTools";
 
-function useBooks(queryParam: QueryParamType<keyof BookTypes> = {}) {
+function useBooks(queryParam: QueryParamProps = {}) {
   const query = queryParam
     ? `/books?${handleQueryParam(queryParam)}`
     : "/books";
   const { data, error }: SWRResponse<BookTypes[]> = useSWR(query);
+
   return {
     books: data,
     isLoading: !error && !data,
@@ -13,18 +15,36 @@ function useBooks(queryParam: QueryParamType<keyof BookTypes> = {}) {
   };
 }
 
-export function useBook(
-  id: string | number,
-  queryParam: QueryParamType<keyof BookTypes> = {}
-) {
+export function useBook(id: string | number, queryParam: QueryParamProps = {}) {
   const query = queryParam
     ? `/books/${id}?${handleQueryParam(queryParam)}`
     : `/books/${id}`;
   const { data, error }: SWRResponse<BookTypes> = useSWR(query);
+
   return {
     book: data,
     isLoading: !error && !data,
     isError: error,
+  };
+}
+
+export function useBooksInfinite(queryParam: QueryParamInfiniteProps) {
+  const { offset = 0, ...other } = queryParam;
+  const { data, error, size, setSize }: SWRInfiniteResponse<BookTypes> =
+    useSWRInfinite((index: number, previous: BookTypes[]) => {
+      if (previous && !previous.length) return null;
+      return `/books?${handleQueryParam({
+        offset: index * other.limit! + offset!,
+        ...other,
+      })}`;
+    });
+
+  return {
+    books: data,
+    isLoading: !error && !data,
+    isError: error,
+    setSize,
+    size,
   };
 }
 
@@ -37,5 +57,11 @@ export interface BookTypes {
   publisher: string;
   title: string;
 }
+
+type QueryParamProps = QueryParamType<keyof BookTypes>;
+
+type QueryParamInfiniteProps = {
+  limit: number;
+} & Omit<QueryParamProps, "limit">;
 
 export default useBooks;
